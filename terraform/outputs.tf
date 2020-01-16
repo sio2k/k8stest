a6caf1b3d442beeff627bfd95bd498536cb6c7c1
Sandeep Kotha <akotha31@gmail.com>
2020-01-16 20:39:13 +0000

    provision k8s defaults

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,14 +6,6 @@ output "host" {
   value = azurerm_kubernetes_cluster.aks.kube_config[0].host
 }
 
-output "username" {
-  value = azurerm_kubernetes_cluster.aks.kube_config[0].username
-}
-
-output "password" {
-  value = azurerm_kubernetes_cluster.aks.kube_config[0].password
-}
-
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
 }
